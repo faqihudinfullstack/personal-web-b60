6fe9cc6c545187723498f4248228a318442ad27f
faqihudin <abdurrahmanfaqih73@gmail.com>
2025-02-11 11:26:06 +0700

    express and handlebars integrated

--- server.js
+++ server.js
@@ -2,23 +2,46 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "hbs");
+
+app.use(express.static("Assets"));
+
+// HALAMAN HOME
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  // res.send("Hello express! This is homepage");
+  res.render("index");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// CONTAC ME
+app.get("/contact", (req, res) => {
+  // res.send("Hello express! This is homepage");
+
+  res.render("contact");
+});
+
+// Blog
+app.get("/clog", (req, res) => {
+  // res.send("Hello express! This is homepage");
+
+  res.render("blog");
+});
+
+// REQUEST PARAMS
+
+app.get("/about/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Hello ini halaman tentang ${id}`);
 });
 
 // REQUEST QUARY
 
-// app.get("/blog", (req, res) => {
-//   const title = req.query.title;
-//   const author = req.query.author;
+app.get("/blog", (req, res) => {
+  const { title, author, year } = req.query;
 
-//   res.send(`ini halaman blog ${title} dengan author ${author}`);
-// });
+  res.send(`ini halaman blog ${title} dengan author ${author} tahun ${year}`);
+});
 
-// app.listen(prompt, () => {
-//   console.log(`My personal web app listening on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`My personal web app listening on port ${port}`);
+});
