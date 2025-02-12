55af285c665740d6b029120443a8f57f05e3c915
faqihudin <abdurrahmanfaqih73@gmail.com>
2025-02-12 21:09:07 +0700

    update 12 februari 1. layouting dan navbar menggunakan parsials membuat helper untuk membandingkan variabel dan keperluan waktu 3. memisahkan blog list dan create blog 4. menjalankan fungsi submit blog menggunakan method post

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const hbs = require("hbs");
+const path = require("path");
+
+const { formatDateToWIB, getRelativeTime } = require("./utils/time");
 
 app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "./views"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("Assets"));
+
+hbs.registerPartials(__dirname + "/views/partials", function (err) {});
+hbs.registerHelper("equal", function (a, b) {
+  return a === b;
+});
+
+hbs.registerHelper("formatDateToWIB", formatDateToWIB);
+hbs.registerHelper("getRelativeTime", getRelativeTime);
+
+let blogs = [];
 
 // HALAMAN HOME
 
 app.get("/", (req, res) => {
   // res.send("Hello express! This is homepage");
   res.render("index");
+  console.log("test");
 });
 
 // CONTAC ME
@@ -20,11 +38,52 @@ app.get("/contact", (req, res) => {
   res.render("contact");
 });
 
-// Blog
-app.get("/clog", (req, res) => {
+// Blog List
+app.get("/blog", (req, res) => {
+  console.log(blogs);
+  res.render("blog-list", { blogs: blogs });
+});
+
+//Create Blog Page
+app.get("/blog-create", (req, res) => {
   // res.send("Hello express! This is homepage");
 
-  res.render("blog");
+  res.render("blog-create");
+});
+
+// Submit New Blog
+app.post("/blog-create", (req, res) => {
+  const { title, content } = req.body; // title dan content adalah properti milik req.body
+  console.log("judulnya adalah", title);
+  console.log("contentnya :", content);
+
+  let image = "https://picsum.photos/200/150";
+
+  let newblog = {
+    title: title,
+    content: content,
+    image: image,
+    author: "M Abdurrahman Faqihudn",
+    postedAt: new Date(),
+  };
+
+  blogs.push(newblog);
+
+  res.redirect("/blog");
+});
+
+// Blog Detail
+app.get("/blog/:id", (req, res) => {
+  // res.send("Hello express! This is homepage");
+
+  res.render("blog-detail");
+});
+
+// Testimonials
+app.get("/testimonials", (req, res) => {
+  // res.send("Hello express! This is homepage");
+
+  res.render("testimonials");
 });
 
 // REQUEST PARAMS
---
+++ utils/time.js
@@ -0,0 +1,68 @@
+function formatDateToWIB(date) {
+  //   let date = new Date();
+  // 01 Feb 2025 11:25 WIB
+  let monthList = [
+    "jan",
+    "feb",
+    "Mar",
+    "Apr",
+    "Mei",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Okt",
+    "Nov",
+    "Dec",
+  ];
+
+  let day = date.getDate().toString().padStart(2, "0");
+  let month = monthList[date.getMonth()];
+  let year = date.getFullYear();
+
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+
+  let formattedDate = `${day} ${month} ${year} ${hours}:${minutes} WIB`;
+
+  return formattedDate;
+}
+function getRelativeTime(postTime) {
+  let now = new Date();
+  console.log("WAKTU SEKARANG :", now);
+
+  console.log("WAKTU user post :", postTime);
+
+  let diffTime = now - postTime;
+  console.log("selisih waktu :", diffTime);
+
+  let diffInSeconds = Math.floor((now - postTime) / 1000);
+  console.log("selisih detik", diffInSeconds);
+
+  if (diffInSeconds < 60) {
+    return `${diffInSeconds} seconds ago`;
+  }
+
+  let diffInMinutes = Math.floor(diffInSeconds / 60);
+
+  if (diffInMinutes < 60) {
+    return `${diffInMinutes} minutes ago`;
+  }
+
+  let diffInHours = Math.floor(diffInMinutes / 60);
+  if (diffInHours < 24) {
+    return `${diffInHours} hours ago`;
+  }
+
+  let diffInDays = Math.floor(diffInHours / 24);
+  if (diffInDays < 30) {
+    return `${diffInDays} days ago`;
+  }
+
+  let diffInMonth = Math.floor(diffInDays / 30);
+  return `${diffInMonth} month${diffInMonth === 1 ? "s" : ""} ago`;
+}
+module.exports = {
+  formatDateToWIB,
+  getRelativeTime,
+};
